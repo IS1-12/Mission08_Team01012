220dd036d382642aa562ecaf29b1d08c43f0eb32
jforellana <mrjfob@byu.edu>
2024-02-26 10:14:35 -0700

    Add project files.

---
+++ Mission08_Team01012/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission08_Team01012.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
